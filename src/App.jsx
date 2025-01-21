445886e9b79f70ebc34252d5d24c131a12571884
PythonMonty <andrewnovosel90@gmail.com>
2025-01-21 18:34:24 -0500

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import './App.css';
 import { useState, React, useEffect } from 'react';
+import './App.css';
 import axios from 'axios';
 import CompletedList from './components/CompletedList';
 import EditTodo from './components/Edit';
