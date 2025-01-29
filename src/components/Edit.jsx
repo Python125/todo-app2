68a3ee26af74eda4f384c74b59aa09dcc67ae081
PythonMonty <andrewnovosel90@gmail.com>
2025-01-28 20:01:47 -0500

    Placed Intl.DateTimeFormat back

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 function EditTodo({ todo, onSave, onCancel }) {
     const [editValue, setEditValue] = useState(todo.name);
-    const [editDueDate, setEditDueDate] = useState(new Date(todo.dueDate));
+    const [editDueDate, setEditDueDate] = useState(new Date(todo.dueDate).toISOString());
 
     const editTodo = (e) => {
         setEditValue(e.target.value);
@@ -17,10 +17,13 @@ function EditTodo({ todo, onSave, onCancel }) {
         if (!editValue.trim()) return; // This makes sure that their are characters in the input field
         if (!editDueDate) return;
 
-        onSave(todo.id, editValue, editDueDate);
-        console.log(editDueDate);
-    }
+        const dueDateString = new Date(editDueDate);
+        const userDate = new Intl.DateTimeFormat('en-US', { dateStyle: 'short', timeStyle: 'short', timeZone: 'America/New_York', hour12: true }).format(dueDateString);
+    
 
+        onSave(todo.id, editValue, userDate);
+        console.log(userDate);
+    }
     return (
         <form onSubmit={submitEditedTodo}>
             <input type="text" value={editValue} onChange={editTodo} />
